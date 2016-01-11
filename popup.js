ce17c52d4c4e75c32e3935c7f31ca884d662f58f
Bryan Jennings <bryanjenningz@gmail.com>
2016-01-10 17:11:44 -0800

    Users can download their saved words

--- popup.js
+++ popup.js
@@ -1,7 +1,16 @@
+var wordSelections;
+
+function downloadSelections() {
+  $(this).attr('href', $(this).attr('href') + wordSelections.join(','));
+}
+
 function displaySelections(selections) {
   var appendLi = (text) => $('<li>').text(text).appendTo('#savedTexts');
   selections.forEach(appendLi);
+  wordSelections = selections.map(s => s.replace(/,/g, '...'));
 }
+
+$('#downloadSelections').click(downloadSelections);
 
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
   chrome.tabs.sendMessage(tabs[0].id, 'selections', displaySelections);
